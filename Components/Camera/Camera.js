7f479808fd00bcc162b36038c816ea8ef2cdf0df
juanpablo <jpmartinez7480@gmail.com>
2018-12-11 22:53:30 -0300

    compotente camaraResult creado y funcionando, falta acción de botones

--- Components/Camera/Camera.js
+++ Components/Camera/Camera.js
@@ -1,33 +1,89 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native';
 import {Camera, Permissions, FileSystem, MediaLibrary} from 'expo';
 import {Button} from 'react-native-paper';
+import CameraResult from '../Camera/CameraResult.js';
 
 export default class CameraView extends Component{
-  static navigationOptions = {header:null};
+static navigationOptions = {header:null};
 
   state = {
     hasCameraPermission: null,
     hasCameraRollGranted:null,
     type: Camera.Constants.Type.back,
+    autoFocus: 'on',
+    flash: 'off',
     newPhotos: false,
-    ratio:'16:9'
+    ratio:'16:9',
+    photo:null,
+    modaVisible: false
   };
 
   snap = () => {
     console.log("sadasdasd")
+    let image
     if(this.camera){
-      this.camera.takePictureAsync({quality:1.0,base64:true,onPictureSaved:this.onPictureSaved});
+      this.camera.takePictureAsync({quality:1.0,base64:true,skipProcessing:true,onPictureSaved:this.onPictureSaved});
     }
+
   }
 
   onPictureSaved = async photo => {
-    console.log(photo.base64)
     const asset = await MediaLibrary.createAssetAsync(photo.uri);
     MediaLibrary.createAlbumAsync('Ecopuntos',asset)
     .then(() => {console.log("creado")})
     .catch(err => {console.log('err'),err})
-    this.setState({newPhotos:true});
+    this.setState({newPhotos:true, photo:photo});
+
+  }
+
+  renderBottomBarCamera(){
+    return(
+    <View style = {styles.bottomBarCamera}>
+      <TouchableOpacity style={{alignSelf: 'center'}} onPress={this.snap}>
+        <Image source = {require('../../assets/camera.png')} style= {styles.bottomButton}/>
+      </TouchableOpacity>
+    </View>
+  )
+  }
+
+  renderBottomCameraResult(){
+    return(
+      <View style = {styles.bottomBarCameraResult}>
+        <TouchableOpacity onPress = {this.confirmPicture} style = {{marginRight:10}}>
+          <Image source = {require('../../assets/check.png')} style = {styles.bottomButton} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress = {this.deletePicture}>
+          <Image source = {require('../../assets/delete2.png')} style = {styles.bottomButton} />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  renderPhoto2(){
+    return(
+      <View>
+        <Image source = {{uri:this.state.photo.uri}} style = {{height:'100%',width:null,resizeMode:'cover'}}/>
+        {this.renderBottomCameraResult()}
+      </View>
+    );
+  }
+
+  renderCamera(){
+    return(
+        <Camera style={{ flex: 1 }} ref={ref => {this.camera = ref;}} autoFocus = {this.state.autoFocus} flashMode = {this.state.flash}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-evenly'
+          }}>
+          {this.renderBottomBarCamera()}
+        </View>
+      </Camera>
+    )
   }
 
    async componentWillMount(){
@@ -44,27 +100,34 @@ export default class CameraView extends Component{
        return <Text>No access to camera</Text>;
      } else {
        return (
-         <View style={{ flex: 1 }}>
-           <Camera style={{ flex: 1 }} ref={ref => {this.camera = ref;}} >
-             <View
-               style={{
-                 flex: 1,
-                 backgroundColor: 'transparent',
-                 flexDirection: 'row',
-                 alignItems: 'center',
-                 justifyContent: 'space-evenly'
-               }}>
-               <TouchableOpacity
-                 style={{
-                   alignSelf: 'flex-end',
-                 }}
-                 onPress={this.snap}>
-                 <Image source = {require('../../assets/camera.png')} style= {{width: 48,height: 48, marginBottom: 20}}/>
-               </TouchableOpacity>
-             </View>
-           </Camera>
+          <View style={{ flex: 1 }}>
+           {this.state.photo ? this.renderPhoto2() : this.renderCamera()}
          </View>
        );
      }
    }
  }
+
+
+const styles = StyleSheet.create({
+  bottomBarCamera:{
+    backgroundColor: 'transparent',
+    alignSelf: 'flex-end',
+    flex:0.1,
+    flexDirection: 'row'
+  },
+  bottomBarCameraResult:{
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom:0,
+    alignSelf: 'center',
+    flex:0.12,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  bottomButton:{
+    width: 32,
+    height: 32,
+    marginBottom: 20
+  }
+})
